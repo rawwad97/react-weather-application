9ff5234d9e94e54a0ebe19ab26c63e53853da213
rawwad97 <117926064+rawwad97@users.noreply.github.com>
2022-12-24 00:43:53 -0600

    Added the GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        This project is coded by{" "}
+        <a href="https://cool-florentine-1729b5.netlify.app" target="_blank">
+          Rawan Awwad
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/rawwad97/react-weather-application"
+          target="_blank"
+        >
+          open sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
